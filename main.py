705a2b7e9d1a4905d87dc4dc7b55e0b021375992
sanjaraT <sanjaratunola@gmail.com>
2026-01-29 15:40:27 +0600

    Data Distribution

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import numpy as np 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv("Combined Data.csv")
 print(df.head())
+
+
+#Data Distribution
+plt.figure(figsize=(6,5))
+sns.countplot(x=df['status'])
+plt.xticks(rotation=45)
+plt.title("Class Distribution")
+plt.show()
+
