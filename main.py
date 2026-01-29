60e18c86750e2a008578862463220a892b8d596c
sanjaraT <sanjaratunola@gmail.com>
2026-01-29 15:46:58 +0600

    Data Cleaning

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re
 
 df = pd.read_csv("Combined Data.csv")
-print(df.head())
-
+# print(df.head())
 
 #Data Distribution
 plt.figure(figsize=(6,5))
 sns.countplot(x=df['status'])
 plt.xticks(rotation=45)
 plt.title("Class Distribution")
-plt.show()
+# plt.show()
+
+df = df.dropna(subset=['statement'])
+
+
+#Data Cleaning
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    return text.strip()
+
+df['clean_text'] = df['statement'].apply(clean_text)
+print(df.head())
 
